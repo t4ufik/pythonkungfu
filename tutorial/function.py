9f89aef25544d1185157c1a49bcd8f7c374f24ac
a903s <49035root@gmail.com>
2013-06-16 00:33:11 +0700

    Adding Function Tutorial

---
+++ tutorial/function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+def cetak(anu):
+	print anu;
+
+def tambah(arg1, arg2, arg3):
+	print arg1 + arg2 + arg3;
+
+def kurang(arg1, arg2):
+	print arg1 - arg2;
+
+def angkaKuadrat(arg):
+	return arg **2;
+
+def angkaDiKaliDua(arg):
+	return arg *2;
+
+tambah(3, 4, 3); #7
+kurang(2, 5); #-3
+angkaGue = angkaKuadrat(angkaDiKaliDua(2));
+cetak(angkaGue);	
+# smart = tambah(kurang(3, 2), 2, 2);
+# cetak(smart);	
