441e0d514236ea14143365556739513a8dd5afe7
a903s <49035root@gmail.com>
2013-06-16 00:31:57 +0700

    Adding Collection Tutorial

---
+++ tutorial/collection.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+asbak = ["Abu", "Rokok", "Asbak"];
+
+print asbak;
+
+for i if __name__ == '__main__':
+	main()
--- tutorial/variablesAndTypes.py
+++ tutorial/variablesAndTypes.py
@@ -9,6 +9,10 @@ myfloat = float(10.0)
 # Integer
 myint = 20
 
+# addition
+add = float(myfloat + myint)
+
 print "String: %s" % mystring
 print "Float: %d" % myfloat
 print "Integer: %d" % myint
+print "addition: %d" % add
